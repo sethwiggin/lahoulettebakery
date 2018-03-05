561b9a97a5dfb0bf52a72dc1a51305ab5725fdb1
Andrei Zvonkov <andrei@appnovation.com>
2018-03-05 18:53:51 -0500

    lahoulettebakery site

---
+++ README.md
@@ -0,0 +1,4 @@
+## La Houlette de vie Bakery
+
+
+Website for a Bakery
---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Scroll customizations
+
+smoothScroll.init({
+  offset: 40,
+  speed: 900,
+  easing: 'easeInOutCubic'
+});
+
+gumshoe.init({
+  offset: 41
+});
